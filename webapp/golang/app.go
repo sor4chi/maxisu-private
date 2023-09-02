3d6f96b55cffb786da4e2ffe571e87eb8acfe9c3
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-09-02 18:12:07 +0900

    feat: add LIMIT

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -567,7 +567,7 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	results := []Post{}
-	err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC", t.Format(ISO8601Format))
+	err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC LIMIT ?", t.Format(ISO8601Format), postsPerPage+1)
 	if err != nil {
 		log.Print(err)
 		return
